7852b56a167d4212274f466583d2e673e168de89
simon <sibo200@gmail.com>
2026-06-03 17:38:15 +0200

    decode bencoded integers

--- src/main.py
+++ src/main.py
@@ -3,11 +3,16 @@ import sys
 
 
 def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
+    first_char = chr(bencoded_value[0])
+    last_char = chr(bencoded_value[-1])
+
+    if first_char.isdigit():
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
+        return str(bencoded_value[first_colon_index+1:], "utf-8")
+    elif first_char == "i" and last_char == "e":
+        return int(bencoded_value[1:-1])
     else:
         raise NotImplementedError("Only strings are supported at the moment")
 
@@ -17,14 +22,7 @@ def main():
 
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
-
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
-
-            raise TypeError(f"Type not serializable: {type(data)}")
-
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        print(json.dumps(decode_bencode(bencoded_value)))
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
